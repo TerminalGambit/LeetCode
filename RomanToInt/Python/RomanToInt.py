a3fe2b5d6ca2e9c0a66001f09120f6141794b258
TerminalGambit <jackmassey.monaco@outlook.com>
2023-12-13 18:21:05 +0100

    Add Roman to Integer conversion logic

--- RomanToInt/Python/RomanToInt.py
+++ RomanToInt/Python/RomanToInt.py
@@ -26,3 +26,17 @@ C can be placed before D (500) and M (1000) to make 400 and 900.
 
 Given a roman numeral, convert it to an integer.
 """
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman = {'I': 1, 'V': 5, 'X': 10,
+                 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        result = 0
+        prev = 0
+        for i in s[::-1]:
+            if roman[i] >= prev:
+                result += roman[i]
+            else:
+                result -= roman[i]
+            prev = roman[i]
+        return result
