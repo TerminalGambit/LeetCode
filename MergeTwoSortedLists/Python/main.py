112c0c282bef003c36c4634128500fa8c93e7f21
TerminalGambit <jackmassey.monaco@outlook.com>
2024-01-29 10:42:37 +0100

    Add mergeTwoLists function to merge two sorted linked lists

--- MergeTwoSortedLists/Python/main.py
+++ MergeTwoSortedLists/Python/main.py
@@ -13,4 +13,42 @@ Return the head of the merged linked list.
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        
+            
+            # Create a dummy node and a pointer to it
+            dummy = ListNode()
+            pointer = dummy
+            
+            # Loop through both lists
+            while list1 and list2:
+                
+                # If the value of list1 is less than list2
+                if list1.val < list2.val:
+                    
+                    # Set the pointer's next node to list1
+                    pointer.next = list1
+                    
+                    # Move list1 forward
+                    list1 = list1.next
+                    
+                # If the value of list2 is less than list1
+                else:
+                    
+                    # Set the pointer's next node to list2
+                    pointer.next = list2
+                    
+                    # Move list2 forward
+                    list2 = list2.next
+                    
+                # Move the pointer forward
+                pointer = pointer.next
+                
+            # If list1 is not empty, set the pointer's next node to list1
+            if list1:
+                pointer.next = list1
+                
+            # If list2 is not empty, set the pointer's next node to list2
+            if list2:
+                pointer.next = list2
+                
+            # Return the dummy node's next node
+            return dummy.next
