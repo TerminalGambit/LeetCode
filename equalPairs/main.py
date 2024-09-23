59b57c3b9a948752c244d6643d2efd85d665f88b
TerminalGambit <jackmassey.monaco@outlook.com>
2024-09-23 22:37:06 +0200

    es yay

--- equalPairs/main.py
+++
@@ -1,7 +0,0 @@
-from typing import List
-
-class Solution:
-    def equalPairs(self, grid: List[List[int]]) -> int:
-        rows = set(grid)
-        cols = set(zip(*grid))
-        return sum(min(rows.count(row), cols.count(col)) for row in rows for col in cols) // 2
